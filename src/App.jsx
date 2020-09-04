ed3c7d3b7529560971194cc4997250c127001e33
Arthur Sakemi <arthursakemi@gmail.com>
2020-09-03 23:29:36 -0300

    initial cleanup and commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global.styles';
+
+function App() {
+  return (
+    <div className='App'>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
