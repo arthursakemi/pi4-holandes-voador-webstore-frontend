123e2a7fd7c488728020b333e0aa8c3a0ed349af
Arthur Sakemi <arthursakemi@gmail.com>
2020-09-05 00:58:52 -0300

    menu item component created

---
+++ src/components/menu-item/MenuItem.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import './menuItem.styles.scss';
+
+const MenuItem = ({ title, imageUrl, history, linkUrl, match }) => (
+  <div className='menu-item' onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default withRouter(MenuItem);
