1a2150383c6ef6bdf49f385d8978cae15a1d792f
Arthur Sakemi <arthursakemi@gmail.com>
2020-09-18 19:16:04 -0300

    paginas de detalhe de produto adicionadas

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/header/Header.component';
 import HomePage from './pages/homepage/Home.page';
 import CadastroProduto from './pages/cadastroProduto/CadastroProduto.page';
 import GaleriaProdutos from './pages/galeriaProdutos/GaleriaProdutos.page';
+import DetalheProduto from './pages/detalhe-produto/DetalheProduto.page';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/produtos/cadastro" component={CadastroProduto} />
+        <Route exact path="/produtos/:id" component={DetalheProduto} />
         <Route path="/produtos" component={GaleriaProdutos} />
         <Route path="/" component={HomePage} />
       </Switch>
