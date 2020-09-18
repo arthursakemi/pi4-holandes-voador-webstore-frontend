cbbc8f933fdbebbd75253f7f2c63fbaca874afd4
mablds <marcelobragalemos@gmail.com>
2020-09-18 01:13:42 -0300

    tela detalhe produto criada

---
+++ src/pages/detaleProduto/DetalheProduto.page.jsx
@@ -0,0 +1,32 @@
+const produtoTeste = {
+    id: 21,
+    nome: 'Camisa Verde',
+    marca: 'Handbook',
+    categoria: 'Masculino',
+    valor: 199,
+    descricao: 'Camisa social de tricoline fio a fio, feita com 100% algodão,barra com curvatura ( curve Hem), manga longa com punho, fechamento botões.',
+    palavrasChave: 'Camisa Algodão Social',
+    p: 10,
+    m: 8,
+    g: 4,
+    unico: 0,
+    imagens: [
+        {id: 30, idProduto: 21, imagem: "https://i.imgur.com/d2tSx65.jpg"},
+        {id: 31, idProduto: 21, imagem: "https://i.imgur.com/jCd04eS.jpg"}
+    ],
+    perguntas: [
+        {
+            id: 36,
+            idProduto: 21,
+            pergunta: "O modelo é Slim-fit?",
+            resposta: "Sim"
+        },
+        {
+            id: 37,
+            idProduto: 21,
+            pergunta: "É 100% algodão?",
+            resposta: "Sim"
+        }
+    ],
+    ativo: true
+  }
