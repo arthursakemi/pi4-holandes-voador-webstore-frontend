53afec2a4c672b570a3b8d34d7f0484d8bac6132
Arthur Sakemi <arthursakemi@gmail.com>
2020-10-14 00:27:16 -0300

    rota de login adicionada e props são passadas para as paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { verify } from 'jsonwebtoken';
+
 import './app.css';
 
 import Header from './components/header/Header.component';
@@ -10,19 +12,47 @@ import GaleriaProdutos from './pages/galeriaProdutos/GaleriaProdutos.page';
 import DetalheProduto from './pages/detalhe-produto/DetalheProduto.page';
 import EdicaoProduto from './pages/edicaoProduto/EdicaoProduto.page';
 import CadastroFuncionario from './pages/cadastroFuncionario/cadastroFuncionario.page';
+import LoginPage from './pages/login/Login.page';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import UserBar from './components/user-bar/UserBar.component';
+
+const initialUser = { nome: '', email: '', cpf: '', cargo: '' };
 
 function App() {
+  const [jwt, setJwt] = useState(sessionStorage.getItem('jwtToken'));
+  const [user, setUser] = useState(initialUser);
+
+  const handleLogOut = (e) => {
+    e.preventDefault();
+    console.log('oi');
+    sessionStorage.clear();
+    setJwt(null);
+    setUser(initialUser);
+  };
+
+  useEffect(() => {
+    try {
+      const jwtPayload = verify(jwt, process.env.REACT_APP_JWT_KEY);
+      setUser(jwtPayload);
+    } catch (e) {}
+    sessionStorage.setItem('jwtToken', jwt);
+  }, [jwt]);
+
+  const appProps = { jwt, setJwt, user };
+
   return (
     <div className="App">
+      <UserBar user={user} handleLogOut={handleLogOut} />
       <Header />
       <Switch>
-        <Route exact path="/produtos/cadastro" component={CadastroProduto} />
-        <Route exact path="/produtos/:id" component={DetalheProduto} />
-        <Route exact path="/produtos/editar/:id" component={EdicaoProduto} />
-        <Route exact path="/produtos/categoria/:categoria" component={GaleriaProdutos} />
-        <Route path="/produtos" component={GaleriaProdutos} />
-        <Route exact path="/backofice/login" />
-        <Route exact path="/backofice/cadastro" component={CadastroFuncionario} />
+        <Route exact path="/produtos/cadastro" render={(props) => <CadastroProduto {...appProps} {...props} />} />
+        <Route exact path="/produtos/:id" render={(props) => <DetalheProduto {...appProps} {...props} />} />
+        <Route exact path="/produtos/editar/:id" render={(props) => <EdicaoProduto {...appProps} {...props} />} />
+        <Route exact path="/produtos/categoria/:categoria" render={(props) => <GaleriaProdutos {...appProps} {...props} />} />
+        <Route path="/produtos" render={(props) => <GaleriaProdutos {...appProps} {...props} />} />
+        <Route exact path="/backofice/login" render={(props) => <LoginPage {...appProps} {...props} />} />
+        <Route exact path="/backofice/cadastro" render={(props) => <CadastroFuncionario {...appProps} {...props} />} />
         <Route exact path="/backofice/usuarios" />
         <Route path="/" component={HomePage} />
       </Switch>
